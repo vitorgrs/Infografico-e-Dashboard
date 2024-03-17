39e41044952a85a4a78294f8b1cf126abcc32e71
Fabio Araujo <82675020+Fabio-araujo-silva@users.noreply.github.com>
2024-03-17 18:39:18 -0300

    loremipsum no entrega de produtos

--- static/javascript/MapaInterativo.js
+++ static/javascript/MapaInterativo.js
@@ -3,7 +3,6 @@ function moveMarker(event) {
     var marker = document.querySelector('.marker');
 
     
-
     var overlay = document.getElementById('overlay');
     var bigCircle = document.getElementById('big-circle');
 
@@ -12,4 +11,17 @@ function moveMarker(event) {
     bigCircle.style.display = 'block';
 }
 
+var p = document.getElementById('myP');
+var div = document.querySelector('.descricaoConclusaoAtividades');
+
+function adicionaP(event) {    
+    var text = document.createTextNode('Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ut harum facere quos, eius dolorem consequuntur consequatur eveniet cumque nam numquam voluptas modi aspernatur ab voluptatibus nobis alias sint corrupti!');
+
+    p.appendChild(text);
+
+    p.className = "fade-in";
+    this.removeEventListener('click', handler);
+}
+
+div.addEventListener('click', adicionaP);
 document.getElementById('marker').addEventListener('click', moveMarker);
