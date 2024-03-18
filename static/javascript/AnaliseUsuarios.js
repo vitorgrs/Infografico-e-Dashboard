f1b37617e50702328f200126b184264f45495238
Vitor Gabriel <vitor.grs2004@gmail.com>
2024-03-18 01:01:00 -0300

    analise por usuário

--- static/javascript/AnaliseUsuarios.js
+++ static/javascript/AnaliseUsuarios.js
@@ -1,3 +1,4 @@
+let primeiroConteiner = true;
 function carregarDadosEExibirGraficos() {
     var caminhoCSV = '../data/dados_csv/tabela2.csv';
     carregarDadosCSV(caminhoCSV, function(dados) {
@@ -28,56 +29,38 @@ function verificarMaiorPorcentagem(nomePerfil ,perfilDados) {
     return { porcentagem: maiorPorcentagem, status: status };
 
 }
-function verificarCordoStatus(status) {
-    let codhex;
-    switch(status) {
-        case 'ENTREGUE (NO PRAZO)':
-            codhex = '#2FF057';
-            break;
-        case 'ENTREGUE (ANTES DO PRAZO DE INICIAR A ATIVIDADE)':
-            codhex = '#2765C1';
-            break;
-        case 'ENTREGUE (COM ATRASO)':
-            codhex = '#FAFE3F';
-            break;
-        case 'NÃO ENTREGUE (PRAZO DA ATIVIDADE NÃO INICIOU)':
-            codhex = '#124A73';
-            break;
-        case 'NÃO ENTREGUE (ATIVIDADE AINDA NO PRAZO)':
-            codhex = '#EF7070';
-            break;
-        case 'NÃO ENTREGUE (ATIVIDADE EM ATRASO)':
-            codhex = '#E7110F';
-            break;
-        default:
-            codhex = '#000000'; 
-            break;
-    }
-    
-    return codhex;
-}
+
 function criarConteinerEExibirGrafico(perfil, dados) {
     var maiorStatusDeEntrega = verificarMaiorPorcentagem(perfil, dados);
 
-    var divUsuarioId = perfil.toLowerCase().replace(/\s+/g, '-');
-
     var divUsuario = document.createElement('div');
-    divUsuario.classList.add('divUsuario');
 
-    divUsuario.id = divUsuarioId;
-    document.getElementById('AnaliseUsuarios').appendChild(divUsuario);
     
+
+    if (primeiroConteiner) {
+        divUsuario.classList.add('carousel-item2'); 
+        divUsuario.classList.add('active'); 
+        primeiroConteiner = false; 
+    }else{
+        divUsuario.classList.add('carousel-item2');
+    }
+
+    document.getElementById('carousel2').appendChild(divUsuario);
+    
+
+    var graficoUsuario = document.createElement('div');
+    graficoUsuario.classList.add('Grafico2');
+    var canvasGrafico = document.createElement('canvas');
+    canvasGrafico.classList.add('Grafico2');
+    graficoUsuario.appendChild(canvasGrafico);
+    divUsuario.appendChild(graficoUsuario);
+
     var nomeDoPerfil = document.createElement('p');
     nomeDoPerfil.classList.add('nomeDoPerfil');
     var texto = document.createTextNode(perfil);
     nomeDoPerfil.appendChild(texto);
 
-    divUsuario.appendChild(nomeDoPerfil);
 
-    var canvasGrafico = document.createElement('canvas');
-    canvasGrafico.classList.add('Grafico2');
-
-    divUsuario.appendChild(canvasGrafico);
 
     var porcentagemEntrega = document.createElement('h1');
     porcentagemEntrega.classList.add('porcentagemEntrega');
@@ -86,14 +69,18 @@ function criarConteinerEExibirGrafico(perfil, dados) {
 
     var descricaoEntrega = document.createElement('p');
     descricaoEntrega.classList.add('descricaoEntrega');
-    descricaoEntrega.textContent = "das atividades foram " + maiorStatusDeEntrega.status;
-
-    divUsuario.appendChild(porcentagemEntrega);
-    divUsuario.appendChild(descricaoEntrega);
+    descricaoEntrega.textContent = "das atividades desse perfil são " + maiorStatusDeEntrega.status;
     
+    var divDescricaoUsuario = document.createElement('div');
+
+    divDescricaoUsuario.appendChild(nomeDoPerfil);
+    divDescricaoUsuario.appendChild(porcentagemEntrega);
+    divDescricaoUsuario.appendChild(descricaoEntrega);
+    divUsuario.appendChild(divDescricaoUsuario);
+
     porcentagemRestante =100-maiorStatusDeEntrega.porcentagem;
 
-    var color= verificarCordoStatus(maiorStatusDeEntrega.status)
+   
     
    
     var context = canvasGrafico.getContext('2d');
@@ -103,8 +90,8 @@ function criarConteinerEExibirGrafico(perfil, dados) {
             datasets: [{
                 data:[maiorStatusDeEntrega.porcentagem, porcentagemRestante],
                 backgroundColor: [
-                    color,
-                    '#1E1F23',
+                    '#00B4FE',
+                    '#1A40F2',
                 ],
             }],
         },
@@ -146,7 +133,10 @@ function carregarDadosCSV(caminho, callback) {
         })
         .catch(error => console.error('Erro ao carregar dados do CSV:', error));
 }
-if (document.getElementById("AnaliseUsuarios")) {
+if (document.getElementById("carousel2")) {
     carregarDadosEExibirGraficos();
+    const scriptCarrosel = document.createElement('script');
+    scriptCarrosel.src = '../static/javascript/carrosel2.js'; 
+    document.body.appendChild(scriptCarrosel);
 }
 
--- static/javascript/carrosel2.js
+++ static/javascript/carrosel2.js
@@ -1,22 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const items = document.querySelectorAll('.carousel-item2');
-    let currentIndex = 0;
-  
-    function showItem(index) {
-      items.forEach(item => item.classList.remove('active'));
-      items[index].classList.add('active');
-    }
-  
-    function goToPrevItem() {
-      currentIndex = (currentIndex - 1 + items.length) % items.length;
-      showItem(currentIndex);
-    }
-  
-    function goToNextItem() {
-      currentIndex = (currentIndex + 1) % items.length;
-      showItem(currentIndex);
-    }
-  
-    document.getElementById('prevButton2').addEventListener('click', goToPrevItem);
-    document.getElementById('nextButton2').addEventListener('click', goToNextItem);
+function carrosel2() {
+  const items = document.querySelectorAll('.carousel-item2');
+  let currentIndex = 0;
+
+  function showItem(index) {
+    items.forEach(item => item.classList.remove('active'));
+    items[index].classList.add('active');
+    const graficos = document.querySelectorAll('canvas');
+    graficos.forEach(grafico => {
+      grafico.style.width = '20rem'; 
+      grafico.style.height = '20rem'; 
   });
+ 
+
+
+  }
+
+  function goToPrevItem() {
+    currentIndex = (currentIndex - 1 + items.length) % items.length;
+    showItem(currentIndex);
+  }
+
+  function goToNextItem() {
+    currentIndex = (currentIndex + 1) % items.length;
+    showItem(currentIndex);
+  }
+
+  document.getElementById('prevButton2').addEventListener('click', goToPrevItem);
+  document.getElementById('nextButton2').addEventListener('click', goToNextItem);
+};
+
+if (document.getElementById("carousel2")) {
+  carrosel2();
+}
